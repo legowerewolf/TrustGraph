b27fc62e014f4bd9e3f6526d147a26b02063dddf
Duncan Gibson <legowerewolf@gmail.com>
2019-03-12 17:00:26 -0700

    Upgrade to gulp 4.0.0

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,18 +3,17 @@ var ts = require('gulp-typescript');
 
 var tsProject = ts.createProject("./tsconfig.json");
 
-gulp.task("build-typescript", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .on("error", (err) => { })
-        .js.pipe(gulp.dest("build"));
-});
+const build_typescript = () => tsProject.src()
+    .pipe(tsProject())
+    .on("error", (err) => { })
+    .js.pipe(gulp.dest("build"));
+
 
 gulp.task("start-watchers", () => {
-    gulp.watch(tsProject.config.include, ["build-typescript"]);
+    gulp.watch(tsProject.config.include, build_typescript);
 })
 
-gulp.task("default-nowatch", ["build-typescript"])
+gulp.task("default-nowatch", build_typescript)
 
-gulp.task("default", ["default-nowatch", "start-watchers"]);
+gulp.task("default", gulp.series("default-nowatch", "start-watchers"));
 
