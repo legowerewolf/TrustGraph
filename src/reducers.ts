a4e375ac3fe726b01b9ce9fb15f8f7183a1051f0
Duncan Gibson <legowerewolf@gmail.com>
2019-04-09 19:59:25 -0700

    Documentation and formatting.

--- src/reducers.ts
+++ src/reducers.ts
@@ -4,16 +4,16 @@ import { arrayIsIdentical } from "./helpers";
 import { GraphAction, Relationship } from "./types";
 
 export const root_reducer = combineReducers({
-    relationships
-})
+	relationships
+});
 
 function relationships(state: Relationship[] = [], action: GraphAction): Relationship[] {
-    switch (action.type) {
-        case Actions.FORM_BOND:
-            return [...state, [action.parent, action.child]];
-        case Actions.BREAK_BOND:
-            return state.filter((relationship) => !arrayIsIdentical(relationship, [action.parent, action.child]))
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case Actions.FORM_BOND:
+			return [...state, [action.parent, action.child]];
+		case Actions.BREAK_BOND:
+			return state.filter((relationship) => !arrayIsIdentical(relationship, [action.parent, action.child]));
+		default:
+			return state;
+	}
 }
